662fd23b1696dbda49b9035957a48a562cecccc0
Matthew Orlando <cogwheel@offbaseproductions.com>
2016-03-08 10:23:38 -0800

    Working demonstration of bug

---
+++ AwaitTest/MyClient.cs
@@ -0,0 +1,126 @@
+﻿//#define FAIL_TEST
+
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AwaitTest
+{
+	public class MyClient
+	{
+		public MyClient()
+		{
+		}
+
+		public struct PageResult
+		{
+			public HttpStatusCode status;
+			public Uri uri;
+			public string content;
+		}
+
+		public async Task<HttpWebResponse> GetResponse(HttpWebRequest request)
+		{
+			//request.CookieContainer = cookies.GetContainer();
+			var response = (HttpWebResponse)await request.GetResponseAsync().ConfigureAwait(false);
+			//foreach (Cookie cookie in response.Cookies)
+			//{
+			//	cookies.SetCookie(cookie);
+			//}
+			return response;
+		}
+
+		private HttpWebRequest CloneRequest(HttpWebRequest orig, Uri uri)
+		{
+			var clone = new HttpWebRequest(uri)
+			{
+				Referer = orig.RequestUri.ToString(),
+				Accept = orig.Accept,
+				AutomaticDecompression = orig.AutomaticDecompression,
+				UserAgent = orig.UserAgent,
+				CookieContainer = orig.CookieContainer,
+				Proxy = orig.Proxy,
+				AllowAutoRedirect = false,
+				MaximumAutomaticRedirections = orig.MaximumAutomaticRedirections
+			};
+
+			foreach (string key in orig.Headers.Keys)
+			{
+				if (!WebHeaderCollection.IsRestricted(key))
+				{
+					clone.Headers.Add(key, orig.Headers[key]);
+				}
+			}
+
+			return clone;
+		}
+
+		private static bool IsRedirect(HttpStatusCode statusCode)
+		{
+			return (int)statusCode / 100 == 3;
+		}
+
+		private async Task<PageResult> GetPage(HttpWebRequest request)
+		{
+			request.AllowAutoRedirect = false;
+
+			HttpWebResponse response = null;
+			try
+			{
+				response = await GetResponse(request);
+				int redirectCount = 0;
+				while (IsRedirect(response.StatusCode))
+				{
+					if (++redirectCount > request.MaximumAutomaticRedirections)
+					{
+						throw new WebException("Too many automatic redirections.", WebExceptionStatus.UnknownError);
+					}
+					var location = response.Headers["Location"];
+
+					request = CloneRequest(request, new Uri(location));
+					response.Dispose();
+					response = await GetResponse(request);
+				}
+
+				using (var stream = response.GetResponseStream())
+				using (var reader = new StreamReader(stream))
+				{
+#if FAIL_TEST
+					return new PageResult
+					{
+						status = response.StatusCode,
+						uri = response.ResponseUri,
+						content = await reader.ReadToEndAsync().ConfigureAwait(false)
+					};
+#else
+					var result = new PageResult
+					{
+						status = response.StatusCode,
+						uri = response.ResponseUri,
+						content = await reader.ReadToEndAsync().ConfigureAwait(false)
+					};
+					return result;
+#endif
+				}
+			}
+			finally
+			{
+				response?.Dispose();
+			}
+		}
+
+		public Task<PageResult> GetPage(Uri uri)
+		{
+			var request = new HttpWebRequest(uri)
+			{
+				//UserAgent = webUserAgent,
+				AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip
+			};
+
+			return GetPage(request);
+		}
+
+	}
+}
+
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.Net;
+
+namespace AwaitTest.Droid
+{
+	[Activity(Label = "AwaitTest", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += async delegate {
+				button.Enabled = false;
+				button.Text = "Getting page...";
+				var client = new MyClient();
+				try
+				{
+					var response = await client.GetPage(new Uri("http://google.com"));
+					if (response.uri == null || response.status == (HttpStatusCode)0)
+					{
+						button.Text = "Corrupted async result";
+					}
+					else
+					{
+						button.Text = "Success";
+					}
+				}
+				catch
+				{
+					button.Text = "Http request failed (check connection)";
+				}
+				finally
+				{
+					button.Enabled = true;
+				}
+			};
+		}
+	}
+}
+
+
